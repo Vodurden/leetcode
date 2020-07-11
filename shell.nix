657c486ffc08b735ba6aa858dac27c6d1cca351e
Jake Woods <jake@jakewoods.net>
2020-07-11 20:03:12 +1000

    Initial Commit

---
+++ shell.nix
@@ -0,0 +1,14 @@
+let
+  sources = import nix/sources.nix;
+  pkgs = import sources.nixpkgs {};
+in
+
+pkgs.mkShell {
+  nativeBuildInputs = with pkgs; [
+    rustc
+    cargo
+
+    rustfmt
+    unstable.rust-analyzer
+  ];
+}
