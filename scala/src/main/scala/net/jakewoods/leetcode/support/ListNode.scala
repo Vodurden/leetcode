0f983e9d5ee552d3f46ba141ea944e937f86f309
Jake Woods <jake@jakewoods.net>
2020-08-23 16:33:08 +1000

    Support cycles and cycle detection in ListNode
    Some Leetcode challenges introduce cycles in `ListNode`. If we're going to
    write test cases for them then we need `ListNode` not to crash or go into an
    infinite loop when we try to compare them or print them.
    
    This implementation is not pretty, but it gets the job done.

--- scala/src/main/scala/net/jakewoods/leetcode/support/ListNode.scala
+++ scala/src/main/scala/net/jakewoods/leetcode/support/ListNode.scala
@@ -7,48 +7,150 @@ import org.scalacheck.Arbitrary
   *
   *  Only `x` and `next` exist in the leetcode version.
   *  All other methods are to make testing and debugging easier.
+  *
+  * This version is generic to make some of the helper function easier
+  * to implement. For a full mock of the leetcode version see `ListNode`
   */
 final class ListNode(var _x: Int = 0) {
   var next: ListNode = null
   var x: Int = _x
 
-  /** Copy constructor
-    *
-    * Performs a deep clone on the list
-    */
-  def this(other: ListNode) = {
-    this(other.x)
-    if (other.next != null) {
-      this.next = new ListNode(other.next)
-    }
-  }
+  type Index = Int
 
-  def fold[A](acc: A)(f: (A, Int) => A): A = {
+  /** `foldNodes` folds over a list, detecting cycles and counting the index of the current node.
+    *
+    * Cycle detection only applies from `this` to the end of the list. If this node is part of a
+    * longer list and the cycle points to an earlier part of the list some values will repeat until
+    * `this` is traversed again.
+    *
+    * This function is used to derive most other methods, even when it would be more efficient to
+    * specialize them because all methods on this class are used to test relatively small sample
+    * sizes
+    */
+  def foldNodes[A](
+    acc: A,
+    onCycle: (A, Index, ListNode) => A
+  )(f: (A, Index, ListNode) => A): A = {
+    // We need to check for cycles when we fold since
+    // some problems create cycles and we don't want
+    // infinite loops in our tests.
+    val visited = new java.util.IdentityHashMap[ListNode, Unit]()
+
     var result = acc
     var current = this
-    while (current != null) {
-      result = f(result, current.x)
+    var currentIndex = 0
+    while (current != null && !visited.containsKey(current)) {
+      result = f(result, currentIndex, current)
+
+      visited.put(current, ())
       current = current.next
+      currentIndex += 1
     }
+
+    // If we're here and current isn't null then it must be
+    // the node we've cycled to
+    if (current != null) {
+      var cycleIndex = 0
+      var cycleIndexCurrent = this
+      while (!(cycleIndexCurrent eq current)) {
+        cycleIndex += 1
+        cycleIndexCurrent = cycleIndexCurrent.next
+      }
+
+      result = onCycle(result, cycleIndex, current)
+    }
+
     result
   }
 
+  def foldNodes[A](acc: A)(f: (A, Index, ListNode) => A): A = {
+    foldNodes(acc, (x: A, _, _) => x)(f)
+  }
+
+  /** Find the node and index of the list item that matches `predicate`
+    *
+    * @param predicate a function that returns true if this is the node we want to find
+    * @return the index and node that matched the predicate, or None
+    */
+  def findWhereWithIndex(
+    predicate: (Index, ListNode) => Boolean
+  ): Option[(Index, ListNode)] = {
+    foldNodes(None : Option[(Index, ListNode)]) { case (result, index, node) =>
+      if(predicate(index, node) && result == None) {
+        Some((index, node))
+      } else {
+        result
+      }
+    }
+  }
+
+  def findWhere(f: ListNode => Boolean): Option[ListNode] = {
+    findWhereWithIndex { case (_, node) => f(node) }.map(_._2)
+  }
+
   def toList: List[Int] = {
-    fold(List.empty[Int]) { case (acc, value) => acc.appended(value) }
+    foldNodes(List.empty[Int]) { case (acc, _, node) => acc.appended(node.x) }
   }
 
   def length: Int = {
-    fold(0) { case (acc, _) => acc + 1 }
+    foldNodes(0) { case (acc, _, _) => acc + 1 }
   }
 
+  /**
+    * Creates a new linked list with the same values and `next` structure as `this`
+    *
+    * If `this` contains a cycle then the cloned list will also contain a cycle.
+    *
+    * @return the head of the cloned list
+    */
   def deepClone: ListNode = {
-    new ListNode(this)
+    var preHead = new ListNode(-1)
+
+    val onCycle = (resultTail: ListNode, index: Index, _: ListNode) => {
+      // We do `index + 1` because `preHead` is not part
+      // of the result list
+      preHead = preHead.withCycle(index + 1)
+      resultTail
+    }
+
+    foldNodes(preHead, onCycle) { case (resultTail, _, node) =>
+      resultTail.next = new ListNode(node.x)
+      resultTail.next
+    }
+
+    preHead.next
+  }
+
+  /** Get the node at some index
+    */
+  def at(targetIndex: Int): Option[ListNode] = {
+    findWhereWithIndex { case (index, _) => targetIndex == index }.map(_._2)
+  }
+
+  def last: Option[ListNode] = {
+    this.findWhere(node => node.next == null)
+  }
+
+  /** Make the end of this linked list cycle back to a previous node.
+    *
+    * @param cycleTargetIndex: The index of the node to cycle back to
+    */
+  def withCycle(cycleTargetIndex: Int): ListNode = {
+    val target = this.at(cycleTargetIndex).orNull
+    this.last.map(last => last.next = target)
+
+    this
   }
 
   override def equals(other: Any): Boolean = {
     other match {
       case that: ListNode => {
-        x == that.x && Objects.equals(next, that.next)
+        var current = this
+        that.foldNodes(true) { case (acc, _, node) =>
+          val result = acc && current.x == node.x
+          current = current.next
+          result
+        }
       }
 
       case _ => false
@@ -58,7 +160,12 @@ final class ListNode(var _x: Int = 0) {
   override def hashCode(): Int = Objects.hash(x, next)
 
   override def toString(): String = {
-    toList.mkString("=>")
+    val onCycle = (acc: String, index: Index, _: ListNode) =>
+      s"${acc}=>(Cycle to $index)"
+
+    this.foldNodes("", onCycle) { case (acc, _, node) =>
+      s"${acc}=>${node.x}"
+    }.drop(2)
   }
 }
 
--- scala/src/test/scala/net/jakewoods/leetcode/support/ListNodeTest.scala
+++ scala/src/test/scala/net/jakewoods/leetcode/support/ListNodeTest.scala
@@ -22,11 +22,29 @@ object ListNodeTest extends TestSuite with UTestScalaCheck {
       assert(result == expected)
     }
 
-    test("fold") {
-      val input = ListNode.listOf(1,2,3)
-      val result = input.fold(0) { case (acc, x) => acc + x }
-      val expected = 6
-      assert(result == expected)
+    test("foldNodes") {
+      test("sum") {
+        val input = ListNode.listOf(1,2,3)
+        val result = input.foldNodes(0) { case (acc, _, node) => acc + node.x }
+        val expected = 6
+        assert(result == expected)
+      }
+
+      test("onCycle index is correct") {
+        val input = ListNode.listOf(1,2).withCycle(0)
+        val onCycle = (_: Int, index: Int, _: ListNode) => index
+        val result = input.foldNodes(-1, onCycle) { case (acc, _, _) => acc }
+
+        assert(result == 0)
+      }
+
+      test("onCycle deeper index is correct") {
+        val input = ListNode.listOf(1,2,3,4).withCycle(2)
+        val onCycle = (_: Int, index: Int, _: ListNode) => index
+        val result = input.foldNodes(-1, onCycle) { case (acc, _, _) => acc }
+
+        assert(result == 2)
+      }
     }
 
     test("length") {
@@ -57,6 +75,27 @@ object ListNodeTest extends TestSuite with UTestScalaCheck {
       }
     }
 
+    test("at") {
+      val List(a, _, _) = ListNode.nodes(1, 2, 3)
+      val aAtZero = a.at(0)
+      val expected = Option(a)
+      assert(aAtZero == expected)
+    }
+
+    test("withCycle creates a cycle") {
+      val List(a, b, c) = ListNode.nodes(1, 2, 3)
+      a.withCycle(0)
+
+      val aNext = a.next
+      assert(aNext eq b)
+
+      val bNext = b.next
+      assert(bNext eq c)
+
+      val cNext = c.next
+      assert(cNext eq a)
+    }
+
     test("toString") {
       def check(list: ListNode, expected: String) = {
         val result = list.toString
@@ -65,19 +104,39 @@ object ListNodeTest extends TestSuite with UTestScalaCheck {
 
       test("List(0)") - check(ListNode.listOf(0), "0")
       test("List(1,2,3)") - check(ListNode.listOf(1,2,3), "1=>2=>3")
+
+      test("circular list") - check(ListNode.listOf(1,2).withCycle(0), "1=>2=>(Cycle to 0)")
     }
 
     test("deepClone") {
-      val original = ListNode.listOf(1,2,3)
-      val cloned = original.deepClone
+      test("normal list") - {
+        val original = ListNode.listOf(1,2,3)
+        val cloned = original.deepClone
 
-      cloned.next.x = 5
+        cloned.next.x = 5
 
-      val expectedOriginal = ListNode.listOf(1,2,3)
-      assert(original == expectedOriginal)
+        val expectedOriginal = ListNode.listOf(1,2,3)
+        assert(original == expectedOriginal)
 
-      val expectedCloned = ListNode.listOf(1,5,3)
-      assert(cloned == expectedCloned)
+        val expectedCloned = ListNode.listOf(1,5,3)
+        assert(cloned == expectedCloned)
+      }
+
+      test("circular list") - {
+        val original = ListNode.listOf(1,2,3).withCycle(1)
+        val cloned = original.deepClone
+
+        cloned.x = 4
+        cloned.next.x = 5
+        cloned.next.next.x = 6
+        cloned.next.next.next.x = 15
+
+        val expectedOriginal = ListNode.listOf(1,2,3)
+        assert(original == expectedOriginal)
+
+        val expectedCloned = ListNode.listOf(4,15,6)
+        assert(cloned == expectedCloned)
+      }
     }
   }
 }
