17d9939b09716a5d4358e8eb1954dc77e53b3ecb
Jake Woods <jake@jakewoods.net>
2020-08-29 13:11:14 +1000

    Solve "Palindrome Linked List" with a faster solution

--- scala/src/main/scala/net/jakewoods/leetcode/PalindromeLinkedList.scala
+++ scala/src/main/scala/net/jakewoods/leetcode/PalindromeLinkedList.scala
@@ -31,4 +31,56 @@ object PalindromeLinkedList {
 
     true
   }
+
+  def isPalindromeFast(head: ListNode): Boolean = {
+    // Make the first half of the list point in the reverse direction. I.e.:
+    //
+    // - "1->2->3->2->1" becomes "1<-2  3->2->1"
+    // - "1->2->2->1" becomes "1<-2  2->1
+    //
+    // Where `reversed` is the head of the reversed list and `remaining` is the
+    // head of the remaining list
+
+    // When `fast` hits null or the value before null `slow` will be in the middle
+    // of the palindrome
+    var slowPrevious: ListNode = null
+    var slow = head
+    var fast = head
+
+    while(fast != null && fast.next != null) {
+      fast = fast.next.next
+
+      val oldSlow = slow.next
+      slow.next = slowPrevious
+      slowPrevious = slow
+      slow = oldSlow
+    }
+
+    // slow is now "3->2->1" (from middle to end)
+    // slowPrevious is now "2->1" (from middle to start)
+    var midToStart = slowPrevious
+    var midToEnd = slow
+
+    // If `fast` is null we know the loop has an even length since fast advances two at a time.
+    val evenLength = fast == null
+
+    // If the list isn't even we should skip the middle node since it's equal to itself.
+    //
+    // This also means `midToEnd` and `midToStart` should always have the same length after
+    // we apply this change
+    if (!evenLength) {
+      midToEnd = midToEnd.next
+    }
+
+    while (midToEnd != null && midToStart != null) {
+      if(midToEnd.x != midToStart.x) {
+        return false
+      }
+
+      midToEnd = midToEnd.next
+      midToStart = midToStart.next
+    }
+
+    true
+  }
 }
--- scala/src/test/scala/net/jakewoods/leetcode/PalindromeLinkedListTest.scala
+++ scala/src/test/scala/net/jakewoods/leetcode/PalindromeLinkedListTest.scala
@@ -1,29 +1,55 @@
 package net.jakewoods.leetcode
 
+import org.scalacheck.Prop
+import org.scalacheck.Arbitrary.arbitrary
 import utest._
+import utest.UTestScalaCheck
 
 import net.jakewoods.leetcode.support.ListNode
 
-object PalindromeLinkedListTest extends TestSuite {
+object PalindromeLinkedListTest extends TestSuite with UTestScalaCheck {
   import PalindromeLinkedList._
 
-  case class TestCase(input: ListNode, output: Boolean) {
+  case class TestCase(input: ListNode, expected: Boolean) {
     def test(f: ListNode => Boolean) = {
       val result = f(input.deepClone)
-      val expected = output // Re-assignment so `assert` prints the value
+      Predef.assert(
+        result == expected,
+        s"input: ${input}, palindrome: ${result}, expected: ${expected}"
+      )
       assert(result == expected)
     }
   }
 
   val falseCase = TestCase(ListNode.listOf(1,2), false)
   val trueCase = TestCase(ListNode.listOf(1,2,2,1), true)
+  val trueCaseOddLength = TestCase(ListNode.listOf(1,2,3,2,1), true)
   val singleValue = TestCase(ListNode.listOf(1), true)
+  val duoValue = TestCase(ListNode.listOf(5,5), true)
 
   val tests = Tests {
     test("isPalindromeNaive") {
       test("false case") - falseCase.test(isPalindromeNaive)
       test("true case") - trueCase.test(isPalindromeNaive)
+      test("true case odd length") - trueCaseOddLength.test(isPalindromeNaive)
       test("single value") - singleValue.test(isPalindromeNaive)
+      test("duo value") - duoValue.test(isPalindromeNaive)
+    }
+
+    test("isPalindromeFast") {
+      test("false case") - falseCase.test(isPalindromeFast)
+      test("true case") - trueCase.test(isPalindromeFast)
+      test("true case odd length") - trueCaseOddLength.test(isPalindromeFast)
+      test("single value") - singleValue.test(isPalindromeFast)
+      test("duo value") - duoValue.test(isPalindromeFast)
+    }
+
+    test("all algorithms have same input/output mapping") {
+      Prop.forAll(arbitrary[ListNode]) { list =>
+        val naiveResult = isPalindromeNaive(list)
+        val fastResult = isPalindromeFast(list)
+        naiveResult == fastResult
+      }.checkUTest()
     }
   }
 }
